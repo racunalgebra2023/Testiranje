505c2f7ecb192277e186996e4669e3ead41aa93f
Benjamin <racunalgebra@gmail.com>
2023-02-16 18:57:11 +0100

    Test 1

--- app/src/main/java/hr/algebra/testiranje/utils/utils.kt
+++ app/src/main/java/hr/algebra/testiranje/utils/utils.kt
@@ -1,21 +1,25 @@
 package hr.algebra.testiranje.utils
 
 fun getNumber( value : String ) : Int? {
-
+    return try{
+         value.toInt( )
+    } catch ( e : Exception ) {
+        null
+    }
 }
 
 fun multiply( n1 : Int, n2 : Int ) : Int {
-
+    return n1*n2
 }
 
 fun divide( n1 : Int, n2 : Int ) : Int {
-
+    return n1/n2
 }
 
 fun add( n1 : Int, n2 : Int ) : Int {
-
+    return n1+n2
 }
 
 fun subtract( n1 : Int, n2 : Int ) : Int {
-
+    return n1-n2
 }
--- app/src/test/java/hr/algebra/testiranje/ExampleUnitTest.kt
+++ app/src/test/java/hr/algebra/testiranje/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package hr.algebra.testiranje
 
+import hr.algebra.testiranje.utils.*
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +13,56 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val result = add( 3, 17 )
+        //assertEquals( result, 20 )
+        assertTrue( result==20 )
+    }
+
+    @Test
+    fun subtraction_isCorrect() {
+        val result = subtract( 20, 17 )
+        assertTrue( result==3 )
+    }
+
+    @Test
+    fun division_isCorrect() {
+        val result = divide( 20, 10 )
+        assertTrue( result==2 )
+    }
+
+    @Test
+    fun multiplication_isCorrect() {
+        val result = multiply( 20, 10 )
+        assertTrue( result==200 )
+    }
+
+    @Test
+    fun gettingCorrectNumber1( ) {
+        val result = getNumber( "10" )
+        assertTrue( result==10 )
+    }
+
+    @Test
+    fun gettingCorrectNumber2( ) {
+        val result = getNumber( "15" )
+        assertTrue( result==15 )
+    }
+
+    @Test
+    fun gettingCorrectNumber3( ) {
+        val result = getNumber( "56" )
+        assertTrue( result==56 )
+    }
+
+    @Test
+    fun gettingNullForEmptyString( ) {
+        val result = getNumber( "" )
+        assertNull( result )
+    }
+
+    @Test
+    fun gettingNullForNonNumberText( ) {
+        val result = getNumber( "Danas je cetvrtak" )
+        assertNull( result )
     }
 }
